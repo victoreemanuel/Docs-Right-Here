42bda9bc372b84979cb7f919482bc7beb3b1bd1b
Hugo E. Contreras <ehugo209@gmail.com>
2026-03-22 22:55:50 -0300

    Barra lateral esquesda estruturada

--- frontend/docsRightHere/src/app/app.routes.ts
+++ frontend/docsRightHere/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"login", component: LoginPage}
+];
