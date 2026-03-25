e998ac8fa1e550dbe04657da664332df5aa43797
RafaBCosta <rafaelbrancocosta30@gmail.com>
2026-03-25 18:58:33 -0300

    código da tela de cadastro

---
+++ frontend/docsRightHere/src/app/template-lateral-login/template-lateral-login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-lateral-login',
+  imports: [],
+  templateUrl: './template-lateral-login.html',
+  styleUrl: './template-lateral-login.css',
+  standalone: true
+})
+export class TemplateLateralLogin {
+
+}
