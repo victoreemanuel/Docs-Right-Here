898c132c1cbfd4f3c0429beb061aa9beacfd8da2
Vinicius Girato Moresqui <viniciusgiratomoresqui@gmail.com>
2026-03-29 15:38:22 -0300

    Revert "Develop"

--- frontend/docsRightHere/src/app/app.routes.ts
+++ frontend/docsRightHere/src/app/app.routes.ts
@@ -1,10 +1,3 @@
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { LoginPage } from './pages/login-page/login-page';
-import { CadastroPage } from './pages/cadastro-page/cadastro-page';
 
-export const routes: Routes = [
-  {path: "login", component: LoginPage},
-  {path: 'cadastro', component: CadastroPage},
-  {path: '', redirectTo: '/login', pathMatch:'full'}
-];
+export const routes: Routes = [];
--- frontend/docsRightHere/src/app/app.ts
+++ frontend/docsRightHere/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TemplateLateralLogin } from './template-lateral-login/template-lateral-login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TemplateLateralLogin],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
