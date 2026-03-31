46b221a5012bc412ba584645f2770b5c0b89fe24
Vinicius Girato Moresqui <viniciusgiratomoresqui@gmail.com>
2026-03-31 16:33:13 -0300

    Implementação da Funcionalidade do formulario reativo

--- frontend/docsRightHere/src/app/meus-cards/meus-cards.ts
+++ frontend/docsRightHere/src/app/meus-cards/meus-cards.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms'; 
 
 @Component({
   selector: 'app-meus-cards',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],  // CommonModule - Essa função é um recuso do angular para ele ensinar como o angular deve se comportar ao tentar se comunicar com html como fechar ou abrir um botão
   templateUrl: './meus-cards.html',
   styleUrl: './meus-cards.css',
 })
-export class MeusCards {}
+
+
+export class MeusCards {
+  mostrarForm = false;
+
+  meuFormulario = new FormGroup({
+    titulo: new FormControl(''), 
+    tag: new FormControl('')     
+  });
+
+  abrirForm() {
+    this.mostrarForm = true;
+  }
+
+  cancelar() {
+    this.mostrarForm = false;
+  }
+
+  salvarCard() {
+    console.log(this.meuFormulario.value);
+    this.mostrarForm = false; 
+  }
+}
