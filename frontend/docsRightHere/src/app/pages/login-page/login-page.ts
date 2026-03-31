85cd74f6f0f0ae6e66a40e81dfdc51b9c511080b
Hugo E. Contreras <ehugo209@gmail.com>
2026-03-31 14:46:36 -0300

    feat: [FRONT-19] adicionar formulario reativo

--- frontend/docsRightHere/src/app/pages/login-page/login-page.ts
+++ frontend/docsRightHere/src/app/pages/login-page/login-page.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TemplateLateralLogin } from '../../components/template-lateral-login/template-lateral-login';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
@@ -14,15 +14,24 @@ import { RouterLink } from '@angular/router';
 export class LoginPage {
   constructor(private HttpClient: HttpClient){}
 
-  email: string = '';
-  password: string = '';
+  formLogin = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.minLength(6), Validators.required])
+  })
+  // email: string = '';
+  // password: string = '';
 
 
   realizarlogin(){
+    if (this.formLogin.invalid) console.log("Login invalidado");
+
     const credenciais = {
-      login: this.email,
-      password: this.password
+      login: this.formLogin.get('email')?.value, //Duas formas de acessar o valor
+      password: this.formLogin.value.password
     };
+    console.log(credenciais);
+    return;
+
 
     const API_URL = "http://localhost:8080/auth/login";
 
