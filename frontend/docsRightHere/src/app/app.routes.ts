0ad695987bc6f77c0117d2059cb7dd6a26574505
Hugo E. Contreras <ehugo209@gmail.com>
2026-04-01 21:58:23 -0300

    refactor: [FRONT-25] mover o componente dashboard para a pasta components

--- frontend/docsRightHere/src/app/app.routes.ts
+++ frontend/docsRightHere/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './dashboard/dashboard';
 import { MeusCards } from './meus-cards/meus-cards';
 import { LoginForm } from './components/login-form/login-form';
 import { RegisterForm } from './components/register-form/register-form';
 import { Auth } from './pages/auth/auth';
 import { Home } from './pages/home/home';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
 
