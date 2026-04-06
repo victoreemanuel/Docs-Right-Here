78c3721299bd7749e5ef79e1ff067e5e8e7045ef
Vinicius Girato Moresqui <viniciusgiratomoresqui@gmail.com>
2026-04-06 14:17:11 -0300

    Fiz adição da  Validações Reativas e Feedback Visual

--- frontend/docsRightHere/src/app/meus-cards/meus-cards.ts
+++ frontend/docsRightHere/src/app/meus-cards/meus-cards.ts
@@ -1,34 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms'; 
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-meus-cards',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],  // CommonModule - Essa função é um recuso do angular para ele ensinar como o angular deve se comportar ao tentar se comunicar com html como fechar ou abrir um botão
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './meus-cards.html',
   styleUrl: './meus-cards.css',
 })
-
-
 export class MeusCards {
   mostrarForm = false;
 
+
   meuFormulario = new FormGroup({
-    titulo: new FormControl(''), 
-    tag: new FormControl('')     
+    titulo: new FormControl('', [
+      Validators.required, 
+      Validators.minLength(3)
+    ]), 
+    tag: new FormControl('', [
+      Validators.required
+    ])     
   });
 
-  abrirForm() {
-    this.mostrarForm = true;
-  }
+  get titulo() { return this.meuFormulario.get('titulo'); }
+  get tag() { return this.meuFormulario.get('tag'); }
+
+  abrirForm() { this.mostrarForm = true; }
 
   cancelar() {
     this.mostrarForm = false;
+    this.meuFormulario.reset(); 
   }
 
   salvarCard() {
-    console.log(this.meuFormulario.value);
-    this.mostrarForm = false; 
+    if (this.meuFormulario.valid) {
+      console.log(this.meuFormulario.value);
+      this.mostrarForm = false;
+      this.meuFormulario.reset();
+    } else {
+      this.meuFormulario.markAllAsTouched();
+    }
   }
 }
