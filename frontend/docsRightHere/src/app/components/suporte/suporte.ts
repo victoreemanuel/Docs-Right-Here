700beb265dad5f59e51be9587968f2ee84d3e2bf
victoreemanuel <victoremanuelvm.31@hotmail.com>
2026-04-27 16:31:18 -0300

    tela suporte

---
+++ frontend/docsRightHere/src/app/components/suporte/suporte.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suporte',
+  imports: [],
+  templateUrl: './suporte.html',
+  styleUrl: './suporte.css',
+})
+export class Suporte {}
