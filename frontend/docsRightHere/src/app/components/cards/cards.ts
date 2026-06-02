3108377d3a7ae79200c3346fb56378782226036f
Vinicius Girato Moresqui <viniciusgiratomoresqui@gmail.com>
2026-06-02 17:15:48 -0300

    Modificações na parte da pagina

--- frontend/docsRightHere/src/app/components/cards/cards.ts
+++ frontend/docsRightHere/src/app/components/cards/cards.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CommonModule } from '@angular/common'; // CommonModule - Essa função é um recuso do angular para ele ensinar como o angular deve se comportar ao tentar se comunicar com html como fechar ou abrir um botão.
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-//import { CardService } from './card.service';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-meus-cards',
